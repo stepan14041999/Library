ace4e736c387459c3e56442c9043fcc446681d18
Nikita_054 <kotov.nikitaa@gmail.com>
2019-03-11 19:37:52 +0400

    Fix bugs

--- src/main/java/ru/sstu/library/controllers/LibraryController.java
+++ src/main/java/ru/sstu/library/controllers/LibraryController.java
@@ -1,14 +1,20 @@
 package ru.sstu.library.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.sstu.library.service.LibraryService;
 
 @Controller
 public class LibraryController {
+    @Autowired
+    private LibraryService libraryService;
 
     @GetMapping("/")
     public String main(Model model){
+        model.addAttribute("news",libraryService.getAllNews());
+        model.addAttribute("genres",libraryService.getAllGenres());
         return "index";
     }
 }
--- src/main/java/ru/sstu/library/service/LibraryService.java
+++ src/main/java/ru/sstu/library/service/LibraryService.java
@@ -1,11 +1,18 @@
 package ru.sstu.library.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.sstu.library.entities.Genre;
+import ru.sstu.library.entities.News;
 import ru.sstu.library.entities.Order;
 import ru.sstu.library.repos.GenreRepo;
 import ru.sstu.library.repos.NewsRepo;
 import ru.sstu.library.repos.OrderRepo;
 
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class LibraryService {
     @Autowired
     private NewsRepo newsRepo;
@@ -14,5 +21,15 @@ public class LibraryService {
     @Autowired
     private OrderRepo orderRepo;
 
-    public List<>
+    public List<News> getAllNews(){
+        List<News> news=(List<News>) newsRepo.findAll();
+        news.sort((x,y)->-x.getDate_publish().compareTo(y.getDate_publish()));
+        return news;
+    }
+
+    public List<Genre> getAllGenres(){
+        List<Genre> genres=(List<Genre>) genreRepo.findAll();
+        genres.sort(Comparator.comparing(Genre::getName));
+        return genres;
+    }
 }
