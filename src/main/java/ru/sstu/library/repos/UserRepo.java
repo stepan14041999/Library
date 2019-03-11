ade21211f1f741793416998b9a0f6ff7bae9939f
Nikita_054 <kotov.nikitaa@gmail.com>
2019-03-11 19:17:02 +0400

    Added settings for connect to db

--- src/main/java/ru/sstu/library/repos/UserRepo.java
+++ src/main/java/ru/sstu/library/repos/UserRepo.java
@@ -1,7 +1,8 @@
 package ru.sstu.library.repos;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.core.userdetails.User;
+import ru.sstu.library.entities.User;
+
 
 public interface UserRepo extends CrudRepository<User,Integer> {
     User findByLogin(String login);
