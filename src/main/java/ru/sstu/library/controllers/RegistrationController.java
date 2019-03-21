c425551298ac31a1d7fe35118fe2a801094903d2
Nikita_054 <kotov.nikitaa@gmail.com>
2019-03-21 16:43:41 +0400

    Add RegistrationService with registration logic

--- src/main/java/ru/sstu/library/controllers/RegistrationController.java
+++ src/main/java/ru/sstu/library/controllers/RegistrationController.java
@@ -8,25 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.sstu.library.entities.User;
 import ru.sstu.library.service.LibraryService;
+import ru.sstu.library.service.RegistrationService;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private LibraryService libraryService;
+    private RegistrationService registrationService;
     @Autowired
     private BCryptPasswordEncoder encoder;
 
     @PostMapping("/registration")
     public String addUser(User user, Model model){
-        User userFromDb=libraryService.getUserByLogin(user.getLogin());
+        User userFromDb=registrationService.getUserByLogin(user.getLogin());
         if(userFromDb!=null){
             model.addAttribute("message","true");
             model.addAttribute("user",user);
             return "registration";
         }
         user.setPassword(encoder.encode(user.getPassword()));
-        user.setRole(libraryService.getRoleByName("USER"));
-        libraryService.saveUser(user);
+        user.setRole(registrationService.getRoleByName("USER"));
+        registrationService.saveUser(user);
         return "success";
     }
 }
