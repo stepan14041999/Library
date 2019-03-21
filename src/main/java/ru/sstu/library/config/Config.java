1f1900f1d16d2cf6d497c6f71f28c8ab18356a07
Daniil <neverovdan@gmail.com>
2019-03-21 23:49:39 +0400

    Add page login and css file

--- src/main/java/ru/sstu/library/config/Config.java
+++ src/main/java/ru/sstu/library/config/Config.java
@@ -22,7 +22,7 @@ public class Config extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/").permitAll()
+                .antMatchers( "/login**","/image/**","/css/**","/").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
