7bce790801e547db31a224326cf18c6a275e6ea1
Nikita_054 <kotov.nikitaa@gmail.com>
2019-04-09 12:05:32 +0400

    Add OrderController and resolve some issue

--- src/main/java/ru/sstu/library/controllers/GenreController.java
+++ src/main/java/ru/sstu/library/controllers/GenreController.java
@@ -9,22 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.sstu.library.entities.Genre;
 import ru.sstu.library.service.GenreService;
+import ru.sstu.library.service.LibraryService;
 
 @Controller
 @RequestMapping("/genre")
 public class GenreController {
     @Autowired
     private GenreService genreService;
+    @Autowired
+    private LibraryService libraryService;
 
     @GetMapping("/")
     public String getAll(Model model){
         model.addAttribute("books",genreService.getAllBooks());
+        model.addAttribute("genres",libraryService.getAllGenres());
         return "genres";
     }
 
     @GetMapping("/{genre}")
     public String booksByGenre(@PathVariable Genre genre, Model model){
         model.addAttribute("books",genreService.getBookByGenre(genre));
+        model.addAttribute("genres",libraryService.getAllGenres());
         return "genres";
     }
 }
